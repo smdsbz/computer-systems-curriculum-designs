7d2a246ebd671fb89978cbb19cc83b8915b2e40c
smdsbz <694066681@qq.com>
2019-01-27 12:47:37 +0800

    finished char device

--- Design04_char-device-driver/mycharflipdev.c
+++ Design04_char-device-driver/mycharflipdev.c
@@ -9,6 +9,7 @@
 #include <linux/fs.h>       // struct file_operations,
                             // unregister/register_chrdev_region()
 #include <linux/string.h>   // memset()
+#include <linux/uaccess.h>  // copy_to_user(), copy_from_user()
 
 /*
  * TODO:
@@ -32,7 +33,7 @@ MODULE_VERSION("0.1");
 // number of devices to be created, here we only create one for pleasure
 #define MYCHARFLIP_DEV_NUM      3
 
-// debugging mode
+// uncomment the following line to enable verbose mode
 #define MYCHARFLIP_DEBUG
 
 
@@ -54,41 +55,126 @@ struct mycharflip_data_t {
 
 /******* Device Operation Handlers *******/
 
-static int mycharflip_open(struct inode *inode, struct file *file);
-static int mycharflip_release(struct inode *inode, struct file *file);
+static int mycharflip_open(struct inode *, struct file *);
+static int mycharflip_release(struct inode *, struct file *);
+static ssize_t mycharflip_read(struct file *, char __user *, size_t, loff_t *);
+static ssize_t mycharflip_write(struct file *, const char __user *, size_t,
+                                loff_t *);
 
 const struct file_operations mycharflip_file_operations = {
     .owner      = THIS_MODULE,
     .open       = mycharflip_open,
-    .release    = mycharflip_release
+    .release    = mycharflip_release,
+    .read       = mycharflip_read,
+    .write      = mycharflip_write
 };
 
 
 static int mycharflip_open(struct inode *inode, struct file *file) {    // {{{
-    int devno;    // device identifier
-    // make reference to the private data associated to the char-device identified by cdev number
-    devno = inode->i_cdev->dev;
+    int devno = inode->i_cdev->dev;
+    // bind file->private_data to private data space
+    file->private_data = &priv_data_arr[MINOR(devno)];
+    ++opening_count;
 #if defined(MYCHARFLIP_DEBUG)
-    printk(KERN_DEBUG "%s: [%d:%d] Opening device...\n", __func__,
+    printk(KERN_DEBUG "%s: [%d:%d] Opened device...\n", __func__,
             MAJOR(devno), MINOR(devno));
 #endif
-    file->private_data = &priv_data_arr[MINOR(devno)];
-    priv_data_arr[MINOR(devno)].last_input = NULL;
-    ++opening_count;
     return 0;
 }   // }}}
 
 static int mycharflip_release(struct inode *inode, struct file *file) {     // {{{
     struct mycharflip_data_t *priv_data = file->private_data;
-    if (likely(priv_data->last_input != NULL)) {
-        kfree(priv_data->last_input);
-    }
     --opening_count;
+    // NOTE: Keep data!!!
 #if defined(MYCHARFLIP_DEBUG)
     printk(KERN_DEBUG "%s: [%d:%d] Closed device...\n", __func__,
             MAJOR(priv_data->cdev->dev), MINOR(priv_data->cdev->dev));
 #endif
     return 0;
+}   // }}}
+
+static ssize_t mycharflip_read(struct file *file, char __user *user_buf,
+                                size_t count, loff_t *offset) {             // {{{
+    struct mycharflip_data_t *priv_data;
+    long retval;
+    char result;
+    priv_data = file->private_data;
+    if (unlikely(priv_data == NULL)) {
+        printk(KERN_ALERT "%s: No data space attached to module!\n", __func__);
+        return -EFAULT;
+    }
+    if (unlikely(priv_data->last_input == NULL)) {
+        printk(KERN_ALERT "%s: No data space attached to device %d:%d!\n",
+                __func__, MAJOR(priv_data->cdev->dev),
+                MINOR(priv_data->cdev->dev));
+        return -EFAULT;
+    }
+    // check range
+    if (*offset >= 1) {     // if reading is done
+        return 0;
+    }
+    // TODO: Mutex lock
+    if (*(priv_data->last_input) >= 65 && *(priv_data->last_input) <= 90) {     // if is uppercase letter
+        result = *(priv_data->last_input) - 'A' + 'a';
+    } else if (*(priv_data->last_input) >= 97
+                && *(priv_data->last_input) <= 122) {   // if is lowercase letter
+        result = *(priv_data->last_input) - 'a' + 'A';
+    } else {
+        result = *(priv_data->last_input);
+    }
+    // copy to user buffer
+    retval = copy_to_user(user_buf, &result, 1);
+    if (unlikely(retval != 0)) {
+        printk(KERN_ALERT "%s: copy_to_user() failed!\n", __func__);
+        return -EFAULT;
+    }
+    // modify offset
+    *offset += 1;
+    // TODO: Mutex unlock
+#if defined(MYCHARFLIP_DEBUG)
+    printk(KERN_DEBUG "%s: Read '%c' from device %d:%d!\n", __func__,
+            result, MAJOR(priv_data->cdev->dev), MINOR(priv_data->cdev->dev));
+#endif
+    return 1;
+}   // }}}
+
+static ssize_t mycharflip_write(struct file *file, const char __user *user_buf,
+                                size_t count, loff_t *offset) {                     // {{{
+    struct mycharflip_data_t *priv_data;
+    long retval;
+    priv_data = file->private_data;
+    if (unlikely(priv_data == NULL)) {
+        printk(KERN_ALERT "%s: No data space attached to module!\n", __func__);
+        return -EFAULT;
+    }
+    // check range
+    if (*offset >= 1) {     // if buffer, now single char, is full
+        return 0;
+    }
+    // TODO: Mutex lock
+    if (unlikely(priv_data->last_input == NULL)) {
+        priv_data->last_input = kmalloc(1, GFP_KERNEL);
+        if (unlikely(priv_data->last_input == NULL)) {
+            printk(KERN_ALERT "%s: Failed to allocate space for private "
+                    "data!\n", __func__);
+            return -ENOMEM;
+        }
+    }
+    // copy from user buffer, only first char copied, other ignored for now
+    retval = copy_from_user(priv_data->last_input, user_buf, 1);
+    if (unlikely(retval != 0)) {
+        printk(KERN_ALERT "%s: copy_from_user() failed!\n", __func__);
+        return -EFAULT;
+    }
+    // modify offset
+    *offset += 1;
+    // TODO: Mutex unlock
+#if defined(MYCHARFLIP_DEBUG)
+    printk(KERN_DEBUG "%s: Wrote '%c' to device %d:%d!\n", __func__,
+            *(priv_data->last_input), MAJOR(priv_data->cdev->dev),
+            MINOR(priv_data->cdev->dev));
+#endif
+    return 1;
 }   // }}}
 
 
@@ -162,6 +248,10 @@ static int __init mycharflip_init(void) {   // {{{
 #endif
     return 0;   // 0 means healthy
 __JUMPSIGN_mycharflip_init_failed_on_creating_device:
+#if defined(MYCHARFLIP_DEBUG)
+    printk(KERN_DEBUG "%s: In __JUMPSIGN_mycharflip_init_failed_on_creating_de"
+            "vice routine\n", __func__);
+#endif
     for (; idx != -1; --idx) {
         if (likely(priv_data_arr[idx].device != NULL)) {
             device_destroy(class, priv_data_arr[idx].cdev->dev);    // NOTE: it finds the device struct internally, DON'T kfree()!
@@ -174,14 +264,21 @@ __JUMPSIGN_mycharflip_init_failed_on_creating_device:
     }
     unregister_chrdev_region(devno, MYCHARFLIP_DEV_NUM);
 __JUMPSIGN_mycharflip_init_failed_on_registering_devno:
+#if defined(MYCHARFLIP_DEBUG)
+    printk(KERN_DEBUG "%s: In __JUMPSIGN_mycharflip_init_failed_on_registering"
+            "_devno routine\n", __func__);
+#endif
     class_destroy(class);
     class = NULL;
 __JUMPSIGN_mycharflip_init_failed_on_registering_class:
+#if defined(MYCHARFLIP_DEBUG)
+    printk(KERN_DEBUG "%s: In __JUMPSIGN_mycharflip_init_failed_on_registering"
+            "_class routine\n", __func__);
+#endif
     kfree(priv_data_arr);
     priv_data_arr = NULL;
     return (int)retval;
 }   // }}}
-
 
 static void __exit mycharflip_cleanup(void) {   // {{{
     int idx;    // loop var
